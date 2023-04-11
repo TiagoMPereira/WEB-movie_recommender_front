355822d9ad84224ca53d0be8d7ea0f2579762ff1
TiagoMPereira <tiagodemoraispereira@gmail.com>
2023-04-11 19:26:50 -0300

    fix: css percentage

--- cinebusca/src/App.js
+++ cinebusca/src/App.js
@@ -50,23 +50,29 @@ function App() {
             <div style={styles.container}>
               <div style={styles.selectWrapper}>
                 <select id="select1">
-                  <option value="option1">Any</option>
-                  <option value="option2">Option 2</option>
-                  <option value="option3">Option 3</option>
+                  <option value="option1">-</option>
+                  <option value="option2">Action</option>
+                  <option value="option3">Family</option>
+                  <option value="option3">Adventure</option>
+                  <option value="option3">Horror</option>
                 </select>
               </div>
               <div style={styles.selectWrapper}>
                 <select id="select2">
-                  <option value="option1">Any</option>
-                  <option value="option2">Option 2</option>
-                  <option value="option3">Option 3</option>
+                  <option value="option1">-</option>
+                  <option value="option2">Action</option>
+                  <option value="option3">Family</option>
+                  <option value="option3">Adventure</option>
+                  <option value="option3">Horror</option>
                 </select>
               </div>
               <div style={styles.selectWrapper}>
                 <select id="select3">
-                  <option value="option1">Any</option>
-                  <option value="option2">Option 2</option>
-                  <option value="option3">Option 3</option>
+                  <option value="option1">-</option>
+                  <option value="option2">Action</option>
+                  <option value="option3">Family</option>
+                  <option value="option3">Adventure</option>
+                  <option value="option3">Horror</option>
                 </select>
               </div>
             </div>
@@ -84,8 +90,7 @@ function App() {
                 value={width}
                 className='custom-slider'
               ></input>
-              <h4 class = "canto">ANO: {width}</h4>
-              <div class = "tamanho"></div>
+              <h4 class = "canto">YEAR: {width}</h4>
               <div class = "checkbox">
                   <input type="checkbox" class="checkbox-round-year"/>
                 <span>
@@ -101,14 +106,13 @@ function App() {
               <input
                 type='range'
                 onChange={changeHeight}
-                min={1}
+                min={0}
                 max={200}
                 step={1}
                 value={height}
                 className='custom-slider'
               ></input>
               <h4 class = "canto">MIN: {height}</h4>
-              <div class = "tamanho"></div>
               <div class = "checkbox">
                   <input type="checkbox" class="checkbox-round-runtime"/>
                 <span>
@@ -121,21 +125,102 @@ function App() {
             <p className="P-form-title">Language</p>
             <p className="P-form-description">Select the main language of the movie you expect</p>
             <div style={styles.selectWrapper}>
-                <select id="select1">
-                  <option value="option1">Any</option>
-                  <option value="option2">Option 2</option>
-                  <option value="option3">Option 3</option>
+                <select id="select4">
+                  <option value="option1">-</option>
+                  <option value="option2">English</option>
+                  <option value="option3">Portuguese</option>
+                  <option value="option3">Spanish</option>
                 </select>
               </div>
           </div>
         </section>
         <div class = "button_send">
           <h1></h1>
-          <button class = "submit" type="button">RECOMMED</button>
+          <button class="submit" type="button" onClick={handleSubmit}>RECOMMEND</button>
         </div>
       </body>
     </div>
   );
+}
+
+//////////////////////////////////////////////////////////////
+
+function isFormValid() {
+  const genderSelects = document.querySelectorAll('#select1, #select2, #select3');
+  const releaseYearInput = document.querySelector('input[type="range"]');
+  const runtimeInput = document.querySelector('input[type="range"]');
+  const languageSelect = document.querySelector('#select4');
+  const releaseYearCheckbox = document.querySelector('.checkbox-round-year');
+  const runtimeCheckbox = document.querySelector('.checkbox-round-runtime');
+
+  let changesCount = 0;
+
+  if (Array.from(genderSelects).some((select) => select.value !== 'option1')) {
+    changesCount++;
+  }
+
+  if (releaseYearInput.value !== '1900' || releaseYearCheckbox.checked) {
+    changesCount++;
+  }
+
+  if (runtimeInput.value !== '0' || runtimeCheckbox.checked) {
+    changesCount++;
+  }
+
+  if (languageSelect.value !== 'option1') {
+    changesCount++;
+  }
+
+  return changesCount >= 3;
+}
+
+///////////////////////////////////////////////////
+function handleSubmit() {
+  if (!isFormValid()) {
+    alertNew();
+    return;
+  }
+
+}
+////////////////////////////////////////////////
+
+
+function alertNew() {
+  // Crie o elemento de alerta personalizado
+  var alertaPersonalizado = document.createElement("div");
+  alertaPersonalizado.classList.add("custom-alert");
+
+  // Adicione o título
+  var titulo = document.createElement("h2");
+  titulo.classList.add("title");
+  titulo.style.color = "red";
+  titulo.style.fontSize = "36px";
+  titulo.textContent = "MISSING INFORMATION";
+  alertaPersonalizado.appendChild(titulo);
+
+  // Adicione a mensagem
+  var mensagem = document.createElement("p");
+  mensagem.classList.add("message");
+  mensagem.style.color = "white";
+  mensagem.style.fontSize = "20px";
+  mensagem.textContent = "You must provide at least 3 informations about the movie that you expect";
+  alertaPersonalizado.appendChild(mensagem);
+
+  // Adicione o botão de fechar
+  var botaoFechar = document.createElement("button");
+  botaoFechar.classList.add("close-button");
+  botaoFechar.style.backgroundColor = "red";
+  botaoFechar.style.color = "white";
+  botaoFechar.textContent = "OK";
+  alertaPersonalizado.appendChild(botaoFechar);
+
+  // Adicione o evento de click para fechar o alerta
+  botaoFechar.addEventListener("click", function() {
+    alertaPersonalizado.style.display = "none";
+  });
+
+  // Adicione o elemento de alerta personalizado à página
+  document.body.appendChild(alertaPersonalizado);
 }
 
 export default App;
